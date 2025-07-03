ae417c6c43014da823f257ca685c5057d0ce167d
Raul <summertdai@hotmail.com>
2025-07-03 05:14:34 -0300

    MENUCOLOR

--- code/Game.py
+++ code/Game.py
@@ -3,12 +3,13 @@
 import pygame
 
 from code.Menu import Menu
+from code.const import WIN_WIDTH, WIN_HEIGHT
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(800, 500))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self, ):
 
@@ -18,11 +19,7 @@ class Game:
             menu.run()
             pass
 
-            # check for all events
-            # for event in pygame.event.get():
-            #     if event.type == pygame.QUIT:
-            #         pygame.quit()  # Close Window
-            #         quit()  # End pygame
+
 
 
 
